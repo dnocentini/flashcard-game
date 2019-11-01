96173ba3c395051434f7f301872bd2f20cd54eb8
Dinny Nocentini <dinny.nocentini@gmail.com>
2019-11-01 11:17:26 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Flashcard Game
+// * 'player' will pick a subject with a 'click';
+// * 'questionCard' will be random card/question about the subject picked;
+// * 'questionCard' will appear on the left;
+// * 'answerCards' will be six(6) random cards/answers;
+// * 'answerCards' will appear on the right;
+// * only one(1) 'answerCards' will be the 'correctCard';
+// * when a 'answerCards' is picked, the card will flip with a answer: 'correctAnswer || else;
+// * if player 
+
+
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
