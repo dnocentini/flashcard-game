effb59ec098334e5b9d533482a922e9b26d4239b
Dinny Nocentini <dinny.nocentini@gmail.com>
2019-11-04 10:29:49 -0600

    placed index cards images using grid display

--- js/main.js
+++ js/main.js
@@ -12,7 +12,21 @@
 
 
 /*----- constants -----*/
-const multiplication = [{}];
+const multiplication = [
+        {"2x1" : "2"},    
+        {"2x2" : "4"},
+        {"2x3" : "6"},
+        {"2x4" : "8"},
+        {"2x5" : "10"},
+        {"2x6" : "12"},
+        {"2x7" : "14"},
+        {"2x8" : "16"},
+        {"2x9" : "18"},
+        {"2x10" : "20"}
+    ];
+
+    console.log(multiplication[0]);
+
 const division = [{}];
 const addition = [{}];
 const subtraction = [{}];
