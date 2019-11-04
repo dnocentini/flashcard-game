10fb0fdfb0db88dc05c4458e2a3f90f6bef27d6b
Dinny Nocentini <dinny.nocentini@gmail.com>
2019-11-03 21:17:32 -0600

    add another wireframe on README

--- js/main.js
+++ js/main.js
@@ -5,11 +5,23 @@
 // * 'answerCards' will be six(6) random cards/answers;
 // * 'answerCards' will appear on the right;
 // * only one(1) 'answerCards' will be the 'correctCard';
-// * when a 'answerCards' is picked, the card will flip with a answer: 'correctAnswer || else;
-// * if player 
+// * when a 'answerCards' is picked, the card will flip with a answer: 'correctAnswer' || 'else';
+// * if 'player' 'click' on the 'correctAnswer', a 'message' saying "Yay! This is the correct answer!" will appear; 
+// * Return;
+// * if 'player' 'click' on 'else' card, a 'message' saying "Tray again!" will appear;
 
 
 /*----- constants -----*/
+const multiplication = [{}];
+const division = [{}];
+const addition = [{}];
+const subtraction = [{}];
+const JS = [{}];
+
 /*----- app's state (variables) -----*/
+
+
 /*----- event listeners -----*/
+
+
 /*----- functions -----*/
