f4c6b9e54b8bbc31f82e4b4842da2918ec13f817
Dinny Nocentini <dinny.nocentini@gmail.com>
2019-11-04 23:25:11 -0600

    created generateCards func

--- js/main.js
+++ js/main.js
@@ -12,30 +12,68 @@
 
 
 /*----- constants -----*/
-const qCardInPlay
-const aCardsInPlay
-const n1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const n2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const addition = {
+    "2+1" : "3",
+    "2+2" : "4",
+    "2+3" : "5",
+    "2+4" : "6",
+    "2+5" : "7",
+    "2+6" : "8",
+    "2+7" : "9",
+    "2+8" : "10",
+    "2+9" : "11",
+    "2+10" : "12",
+}
 
 
 /*----- app's state (variables) -----*/
-const addCorrectAnswer = (n1[1] + n2[1]);
-const subCorrectAnswer = (n1[1] - n2[1]);
-const multCorrectAnswer = (n1[1] * n2[1]);
-const divCorrectAnswer = (n1[1] / n2[1]);  
+
+
+// var questionCard
+// var aCard
 
 /*----- cached element references -----*/
-messageEl = document.getElementById('message');
-resetGameBtn = document.getElementById('resetGame');
+// messageEl = document.getElementById('message');
+qCardEl = document.getElementById('q1');
+aCard1 = document.getElementById('a1');
+aCard2 = document.getElementById('a2');
+aCard3 = document.getElementById('a3');
+aCard4 = document.getElementById('a4');
+aCard5 = document.getElementById('a5');
+aCard6 = document.getElementById('a6');
 
 
-// /*----- event listeners -----*/
-subjectEl.addEventListener('click', handleClick);
-aCardEl.addEventListener("click", flipCard);
-resetGameBtn.addEventListener('click', init);
+
+// // /*----- event listeners -----*/
+// subjectEl.addEventListener('click', handleClick);
+// aCardEl.addEventListener("click", flipCard);
+// resetGameBtn.addEventListener('click', init);
 
 /*----- functions -----*/
 
-// let total = 4;
+init();
 
-// document.getElementsById('a1').src='assets/IndexCard' + total + '.png"/>';
+function init() {
+    generateCards();
+}
+
+function generateCards(){
+    qCardEl.innerText = "2+2";
+    aCard1.innerText = addition["2+2"];
+    aCard2.innerText = addition["2+3"];
+    aCard3.innerText = addition["2+4"];
+    aCard4.innerText = addition["2+5"];
+    aCard5.innerText = addition["2+6"];
+    aCard6.innerText = addition["2+7"];
+    
+}
+
+// function getRandomNum (min, max) {
+//     return Math.floor(Math.random() * (10 - 1) +1)
+// }
+
+
+
+
+
+ 
