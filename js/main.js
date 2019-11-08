700477babcaf20eb698f112851c7504c3553ec55
Dinny Nocentini <dinny.nocentini@gmail.com>
2019-11-08 11:56:04 -0600

    worked at README

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-// Flashcard Game
-// * 'player' will pick a subject with a 'click';
-// * 'questionCard' will be random card/question about the subject picked;
-// * 'questionCard' will appear on the left;
-// * 'answerCards' will be six(6) random cards/answers;
-// * 'answerCards' will appear on the right;
-// * only one(1) 'answerCards' will be the 'correctCard';
-// * when a 'answerCards' is picked, the card will flip with a answer: 'correctAnswer' || 'else';
-// * if 'player' 'click' on the 'correctAnswer', a 'message' saying 'Yay! This is the correct answer!' will appear; 
-// * Return;
-// * if 'player' 'click' on 'else' card, a 'message' saying 'Tray again!' will appear;
-
 /*----- constants -----*/
 const questions = {}
 
@@ -24,9 +12,9 @@ var operator = '';
 buttonEl = document.getElementById('button');
 menuEl = document.getElementById('menu');
 gameBoardEl = document.getElementById('game-board');
-qCardEl = document.getElementById('q1');
-pScoreEl = document.getElementById('score');
 messageEl = document.getElementById('message');
+pScoreEl = document.getElementById('score');
+qCardEl = document.getElementById('q1');
 
 additionEl = document.getElementById('add');
 subtractionEl = document.getElementById('sub');
