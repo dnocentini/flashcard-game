cd9f940458bfa701bb772f2a53b6a219e9c888bd
Dinny Nocentini <dinny.nocentini@gmail.com>
2019-11-08 00:04:07 -0600

    bug fixes

--- js/main.js
+++ js/main.js
@@ -6,14 +6,12 @@
 // * 'answerCards' will appear on the right;
 // * only one(1) 'answerCards' will be the 'correctCard';
 // * when a 'answerCards' is picked, the card will flip with a answer: 'correctAnswer' || 'else';
-// * if 'player' 'click' on the 'correctAnswer', a 'message' saying "Yay! This is the correct answer!" will appear; 
+// * if 'player' 'click' on the 'correctAnswer', a 'message' saying 'Yay! This is the correct answer!' will appear; 
 // * Return;
-// * if 'player' 'click' on 'else' card, a 'message' saying "Tray again!" will appear;
-
+// * if 'player' 'click' on 'else' card, a 'message' saying 'Tray again!' will appear;
 
 /*----- constants -----*/
 const questions = {}
-
 
 /*----- app's state (variables) -----*/
 var qKey;
@@ -21,17 +19,19 @@ var aKeys = [];
 var score = 0;
 var operator = '';
 
+
 /*----- cached element references -----*/
+buttonEl = document.getElementById('button');
 menuEl = document.getElementById('menu');
 gameBoardEl = document.getElementById('game-board');
 qCardEl = document.getElementById('q1');
-pScore = document.getElementById('score');
+pScoreEl = document.getElementById('score');
+messageEl = document.getElementById('message');
 
 additionEl = document.getElementById('add');
 subtractionEl = document.getElementById('sub');
 multiplicationEl = document.getElementById('mult');
 divisionEl = document.getElementById('divi');
-
 
 aCard1El = document.getElementById('aCard1');
 aCard2El = document.getElementById('aCard2');
@@ -40,60 +40,38 @@ aCard4El = document.getElementById('aCard4');
 aCard5El = document.getElementById('aCard5');
 aCard6El = document.getElementById('aCard6');
 
-aCard1 = document.getElementById('a1');
-aCard2 = document.getElementById('a2');
-aCard3 = document.getElementById('a3');
-aCard4 = document.getElementById('a4');
-aCard5 = document.getElementById('a5');
-aCard6 = document.getElementById('a6');
-
-message = document.getElementById('message');
+a1El = document.getElementById('a1');
+a2El = document.getElementById('a2');
+a3El = document.getElementById('a3');
+a4El = document.getElementById('a4');
+a5El = document.getElementById('a5');
+a6El = document.getElementById('a6');
 
 // // /*----- event listeners -----*/
-// subjectEl.addEventListener('click', handleClick);
-additionEl.addEventListener("click", pickSubject);
-subtractionEl.addEventListener("click", pickSubject);
-multiplicationEl.addEventListener("click", pickSubject);
-divisionEl.addEventListener("click", pickSubject);
+buttonEl.addEventListener('click', returnMenu);
 
+additionEl.addEventListener('click', pickSubject);
+subtractionEl.addEventListener('click', pickSubject);
+multiplicationEl.addEventListener('click', pickSubject);
+divisionEl.addEventListener('click', pickSubject);
 
-aCard1El.addEventListener("click", flipCard);
-aCard2El.addEventListener("click", flipCard);
-aCard3El.addEventListener("click", flipCard);
-aCard4El.addEventListener("click", flipCard);
-aCard5El.addEventListener("click", flipCard);
-aCard6El.addEventListener("click", flipCard);
-
-
+aCard1El.addEventListener('click', clickCard);
+aCard2El.addEventListener('click', clickCard);
+aCard3El.addEventListener('click', clickCard);
+aCard4El.addEventListener('click', clickCard);
+aCard5El.addEventListener('click', clickCard);
+aCard6El.addEventListener('click', clickCard);
 
 /*----- functions -----*/
-
-init();
-
 function init() {
-    menuEl.style.display = "";
-    gameBoardEl.style.display = "none";
+    menuEl.style.display = '';
+    gameBoardEl.style.display = 'none';
     aKeys = [];
-};
+}
 
-function pickSubject(evt){
-    if(evt.currentTarget.id === 'add'){
-        operator = '+';
-    } else if(evt.currentTarget.id === 'sub'){
-        operator = '-';
-    }else if(evt.currentTarget.id === 'mult'){
-        operator = '*';
-    }else {
-        operator = '/';
-    }
+function generateCards() {
+    messageEl.innerText = '';
 
-    menuEl.style.display = "none";
-    gameBoardEl.style.display = "";
-    generateQuestions();
-    generateCards();
-};
-
-function generateCards(){
     var usedResults = [];
     var qKeyPre = getRandomNum(2, 9);
     var qKeyPost = getRandomNum(1, 10);
@@ -117,57 +95,79 @@ function generateCards(){
         aKeys.push(aKey);
         usedResults.push(questions[aKey]);
     }
+
     shuffle(aKeys);
 
-    aCard1.innerText = questions[aKeys[0]];
-    aCard2.innerText = questions[aKeys[1]];
-    aCard3.innerText = questions[aKeys[2]];
-    aCard4.innerText = questions[aKeys[3]];
-    aCard5.innerText = questions[aKeys[4]];
-    aCard6.innerText = questions[aKeys[5]];
+    a1El.innerText = questions[aKeys[0]];
+    a2El.innerText = questions[aKeys[1]];
+    a3El.innerText = questions[aKeys[2]];
+    a4El.innerText = questions[aKeys[3]];
+    a5El.innerText = questions[aKeys[4]];
+    a6El.innerText = questions[aKeys[5]];
 }
 
-function flipCard(evt){
-    if(evt.currentTarget.querySelector('div').innerText === questions[qCardEl.innerText]) {
-        message.innerText = "Yay! You got this."
-        score++;
-        pScore.textContent = `Score: ${score}`;
-        setTimeout(function(){
-            aKeys = [];
-            generateCards();
-        }, 1000);
-    }else{
-        message.innerText = "Ops! Try again."
-        evt.currentTarget.querySelector('div').innerText = "WRONG"
-    }
-    
-}
-
-function getRandomNum(min, max) {
-    return Math.floor(Math.random() * (max - min) + min)
-}
-
-function shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
-  }
-
-function generateQuestions(){
-    for(term1 = 2; term1 <= 9; term1++) {
-        for(term2 = 1; term2 <= 10; term2++) {
+function generateQuestions() {
+    for (term1 = 2; term1 <= 9; term1++) {
+        for (term2 = 1; term2 <= 10; term2++) {
             var result = 0;
-            if(operator === '+'){
-                result = term1 + term2
-            } else if(operator === '-'){
-                result = term1 - term2
-            } else if(operator === '*'){
-                result = term1 * term2
+            if (operator === '+') {
+                result = term1 + term2;
+            } else if (operator === '-') {
+                result = term1 - term2;
+            } else if (operator === '*') {
+                result = term1 * term2;
             } else {
-                result = term1 / term2    
-            }; 
+                result = (term1 / term2).toFixed(1);
+            }
 
             questions[term1.toString() + operator + term2.toString()] = result.toString();
         }  
     }
-}  
+}
+
+function pickSubject(evt) {
+    if (evt.currentTarget.id === 'add') {
+        operator = '+';
+    } else if (evt.currentTarget.id === 'sub') {
+        operator = '-';
+    } else if (evt.currentTarget.id === 'mult') {
+        operator = '*';
+    } else {
+        operator = '/';
+    }
+
+    menuEl.style.display = 'none';
+    gameBoardEl.style.display = '';
+    generateQuestions();
+    generateCards();
+}
+
+function clickCard(evt) {
+    if (evt.currentTarget.querySelector('div').innerText === questions[qCardEl.innerText]) {
+        messageEl.innerText = 'Yay! You got this.';
+        score++;
+        pScoreEl.textContent = `Score: ${score}`;
+        setTimeout(function(){
+            aKeys = [];
+            generateCards();
+        }, 1000);
+    } else {
+        messageEl.innerText = 'Ops! Try again.';
+        evt.currentTarget.querySelector('div').innerText = 'WRONG';
+    }
+}
+
+function returnMenu(evt){
+    init();
+}
+
+function getRandomNum(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+}
 
 
+init();
